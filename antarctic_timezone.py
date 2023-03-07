2990311b9927df9defb1d7889c89f55e6c9b482e
desm <desm80@yandex.ru>
2023-03-07 10:11:38 +0300

    задачки спринта парсинг

--- antarctic_timezone.py
+++ antarctic_timezone.py
@@ -1,13 +1,13 @@
 from time import sleep
 
 # Импортируйте библиотеку requests_cache.
-...
+import requests_cache
 
 TIME_API_URL = 'http://worldtimeapi.org/api/'
 
 if __name__ == '__main__':
     # Создайте сессию, кеширующую результаты загрузки страниц.
-    ...
+    session = requests_cache.CachedSession()
     # Уточнение местоположения.
     vostok_url = TIME_API_URL + 'timezone/Antarctica/Vostok'
     # Загружаем текущее время станции "Восток" пять раз.
@@ -15,10 +15,10 @@ if __name__ == '__main__':
         # После третьей загрузки — очистка кеша.
         if iteration > 2:
             # Очистите кеш.
-            ...
+            session.cache.clear()
         # Загрузите веб-страницу из переменной vostok_url,
         # используя созданную сессию.
-        response = ...
+        response = session.get(vostok_url)
         # Ответ от сервера приходит в формате JSON,
         # поэтому нужно преобразовать его методом .json(),
         # чтобы дальше работа велась с данными, как со словарём.
