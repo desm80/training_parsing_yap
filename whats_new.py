6603f69c87f2cf3656393ec1c4bbf25140d1284d
desm <desm80@yandex.ru>
2023-03-09 15:57:40 +0300

    задачки спринта парсинг

--- whats_new.py
+++ whats_new.py
@@ -1,5 +1,7 @@
 import requests_cache
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+from tqdm import tqdm
 
 WHATS_NEW_URL = 'https://docs.python.org/3/whatsnew/'
 
@@ -8,9 +10,40 @@ if __name__ == '__main__':
     session = requests_cache.CachedSession()
     response = session.get(WHATS_NEW_URL)
     response.encoding = 'utf-8'
-    soup = BeautifulSoup(response.text, 'lxml')
-    # py39 = soup.find_all(id="whatsnew-index")
-    py39 = soup.find_all('span', attrs={'id': 'whatsnew-index'})
+    # Создание "супа".
+    soup = BeautifulSoup(response.text, features='lxml')
 
+    # Шаг 1-й: поиск в "супе" тега section с нужным id. Парсеру нужен только
+    # первый элемент, поэтому используется метод find().
+    main_div = soup.find('section', attrs={'id': 'what-s-new-in-python'})
 
-    print(py39)
+    # Шаг 2-й: поиск внутри main_div следующего тега div с классом toctree-wrapper.
+    # Здесь тоже нужен только первый элемент, используется метод find().
+    div_with_ul = main_div.find('div', attrs={'class': 'toctree-wrapper'})
+
+    # Шаг 3-й: поиск внутри div_with_ul всех элементов списка li с классом toctree-l1.
+    # Нужны все теги, поэтому используется метод find_all().
+    sections_by_python = div_with_ul.find_all('li', attrs={'class': 'toctree-l1'})
+
+    # Печать первого найденного элемента.
+    results = []
+    for section in tqdm(sections_by_python):
+        version_a_tag = section.find('a')
+        href = version_a_tag['href']
+        version_link = urljoin(WHATS_NEW_URL, href)
+        # session = requests_cache.CachedSession()
+        response = session.get(version_link)
+        response.encoding = 'utf-8'
+        soup = BeautifulSoup(response.text, features='lxml')
+        h1 = soup.find('h1')
+        dl = soup.find('dl')
+        dl_text = dl.text.replace('\n', ' ')
+        # На печать теперь выводится переменная dl_text — без пустых строчек.
+        results.append(
+            (version_link, h1.text, dl_text)
+        )
+
+    # Печать списка с данными.
+    for row in results:
+        # Распаковка каждого кортежа при печати при помощи звездочки.
+        print(*row)
